234675a44b4a952781a607d2940eb6f219445477
Kittyboy06 <afsalahmed2006@gmail.com>
2025-12-24 16:35:21 +0530

    chore: Set Vite base path to repository name for GitHub Pages deployment.

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    // Setting base to './' allows the app to be deployed to any subdirectory
-    // (like https://user.github.io/repo-name/) without absolute path issues.
-    base: './'
+    // Use the repository name as the base path for GitHub Pages
+    base: '/New-Year-2025/'
 })
