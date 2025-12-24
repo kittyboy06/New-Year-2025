a7f949d8c9a34a17cb79b69250616f75239c6255
Kittyboy06 <afsalahmed2006@gmail.com>
2025-12-24 16:18:31 +0530

    Changing Base

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    // Setting base to './' allows the app to be deployed to any subdirectory
+    // (like https://user.github.io/repo-name/) without absolute path issues.
+    base: './'
+})
